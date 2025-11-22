0be37e94dfadc7301c27718def0a978c9a4c4e98
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-11-22 10:21:45 -0600

    added in card title and proper view button for each topic entry

--- thread/js/script.js
+++ thread/js/script.js
@@ -1,8 +1,12 @@
 const threadTopicEntries = [
   "What is the best opening to learn?",
   "Does drunk Magnus play better than sober Magnus?",
-  "What is y'all favorite openings?",
+  "What are y'all favorite openings?",
   "Why are there so many bugs in this website?",
+  'Does anyone want to play?',
+  'I hate playing against Thelegend27. He stalls',
+  'Someone needs to ban Carter',
+  'Hi',
 ];
 
 function threadTopics() {
@@ -11,7 +15,7 @@ function threadTopics() {
       .getElementById("topic-card-template")
       .content.cloneNode(true);
 
-      template.querySelector('card-title');
+      template.querySelector('[data-title]').innerText = topic;
 
       document.getElementById('card-list').appendChild(template);
   });
